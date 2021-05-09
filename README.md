9a2aaf0928eda0af11d30b50de7219b7714b4088
unknown <joannasmerea@gmail.com>
2021-05-09 14:50:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+$clone
+$npm i
+
+
+## Server
+cd src
+nodemon server.js
+
+## Client
+npm run start
+
+
+SERVER From scratch
+cd server
+npm init
+npm install express cors body-parser
+npm install nodemon --save-dev
---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const path = require('path');
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+//request the path API and serve static files from the client build folder.
+app.use(express.static(path.join(__dirname, '../client/build')));
+/* app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+}); */
+
+const PORT = process.env.PORT || 5000 ;
+
+
+const pollData = require("./data.json");
+app.get("/poll", function (req, res) {
+  res.send(pollData);
+});
+
+app.post("/poll", function (req, res) {
+  if (req.body) {
+    fs.writeFileSync("data.json", JSON.stringify(req.body));  
+    res.send({
+      message: "Data Saved",
+    });
+  } else {
+    res.status(400).send({
+      message: "Error No Data",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});

2bab88b1f9e9ff9a0d36d02d92d3f3e9276d40d2
unknown <joannasmerea@gmail.com>
2021-05-09 18:05:57 +0200

    unistall react dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Poll from "./components/Poll";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route path="/" exact component={Poll} />
-      </div>
-    </Router>
+    <div >
+      <Poll />
+  </div>
   );
 }
 
